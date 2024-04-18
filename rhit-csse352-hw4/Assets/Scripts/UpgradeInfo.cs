24874e14f92db883e077de834a2ad087e9d373ad
seilerar <seilerar@rose-hulman.edu>
2024-04-18 14:55:53 -0400

    Remove textureName

--- rhit-csse352-hw4/Assets/Scripts/UpgradeInfo.cs
+++ rhit-csse352-hw4/Assets/Scripts/UpgradeInfo.cs
@@ -8,20 +8,13 @@ public class UpgradeInfo : HoverInfo
 {
     float moneyCost;
     int obsidianCost;
-    string textureName;
     List<GameModifier> modifiers;
 
-    public UpgradeInfo(float moneyCost, int obsidianCost, string title, string text, string textureName, List<GameModifier> modifiers) : base(title, text)
+    public UpgradeInfo(float moneyCost, int obsidianCost, string title, string text, List<GameModifier> modifiers) : base(title, text)
     {
         this.moneyCost = moneyCost;
         this.obsidianCost = obsidianCost;
-        this.textureName = textureName;
         this.modifiers = modifiers;
-    }
-
-    public Texture2D GetTexture()
-    {
-        return Resources.Load<Texture2D>(textureName);
     }
 
     public override string GetText()
--- rhit-csse352-hw4/Assets/Scripts/UpgradeManager.cs
+++ rhit-csse352-hw4/Assets/Scripts/UpgradeManager.cs
@@ -11,7 +11,6 @@ public class UpgradeManager : UIUpdatable
             0, 0,
             "Upgrade 0",
             "This is an upgrade!",
-            "TempTexture",
             new List<GameModifier>
             {
             }),
@@ -29,15 +28,14 @@ public class UpgradeManager : UIUpdatable
     protected override void OnStart()
     {
         upgrades.Clear();
-        for (int i = 0; i < 40; i++)
+        for (int i = 0; i < upgradeInfos.Count; i++)
         {
             GameObject upgrade = Instantiate(upgradePrefab);
             upgrades.Add(upgrade);
-            var info = upgradeInfos[0];
+            var info = upgradeInfos[i];
             var image = upgrade.GetComponent<Image>();
-            //upgrade.GetComponent<Button>().onClick = ;
             upgrade.GetComponent<Upgrade>().info = info;
-            image.sprite = Sprite.Create(info.GetTexture(), image.sprite.rect, image.sprite.pivot, image.sprite.pixelsPerUnit);
+            image.sprite = Sprite.Create(Resources.Load<Texture2D>($"Upgrade{i}"), image.sprite.rect, image.sprite.pivot, image.sprite.pixelsPerUnit);
             upgrade.transform.SetParent(transform);
         }
     }
